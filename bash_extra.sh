26ffbf432a573a393200a79bc1993d9d8deebfcd
John Demme <john.demme@microsoft.com>
2025-04-23 21:40:03 +0000

    extra lines for .bashrc

---
+++ bash_extra.sh
@@ -0,0 +1,9 @@
+export LM_LICENSE_FILE=4100@msfpgalic.corp.microsoft.com:40004@wanlic.svceng.com
+
+export VISUAL=vim
+export EDITOR=vim
+export CC=clang
+export CXX=clang++
+export CXXFLAGS=-fuse-ld=lld
+
+eval "$(oh-my-posh init bash --config ~/.files/.myposhtheme.omp.json)"
