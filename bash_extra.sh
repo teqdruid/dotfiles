60c7962683f1da68318c4f8c34418d2e976e7fe2
John Demme <john.demme@microsoft.com>
2025-12-09 21:34:03 +0000

    misc

--- bash_extra.sh
+++ bash_extra.sh
@@ -1,9 +1,10 @@
 export LM_LICENSE_FILE=4100@msfpgalic.corp.microsoft.com:40004@wanlic.svceng.com
+export SALT_LICENSE_SERVER="4100@msfpgalic.corp.microsoft.com"
 
 export VISUAL=vim
 export EDITOR=vim
-export CC=clang
-export CXX=clang++
-export CXXFLAGS=-fuse-ld=lld
+#export CC=clang
+#export CXX=clang++
+#export CXXFLAGS=-fuse-ld=lld
 
 eval "$(oh-my-posh init bash --config ~/.files/.myposhtheme.omp.json)"
